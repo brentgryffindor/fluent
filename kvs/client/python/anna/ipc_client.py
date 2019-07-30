83fe687fff2e3f4ef389c81d5a668095b6cb7c94
cw75 <cgwu0530@gmail.com>
2019-07-30 06:02:25 +0000

    fix

--- kvs/client/python/anna/ipc_client.py
+++ kvs/client/python/anna/ipc_client.py
@@ -26,8 +26,8 @@ import zmq
 import time
 
 class IpcAnnaClient:
-    def __init__(self, thread_id = 0):
-        self.context = zmq.Context(1)
+    def __init__(self, ctx, thread_id = 0):
+        self.context = ctx
 
         self.get_response_address = GET_RESPONSE_ADDR_TEMPLATE % thread_id
         self.put_response_address = PUT_RESPONSE_ADDR_TEMPLATE % thread_id
