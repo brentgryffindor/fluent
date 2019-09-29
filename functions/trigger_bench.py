0058f91fbda5413255ba1cba7d0365108222302b
cw75 <cgwu0530@gmail.com>
2019-09-29 06:38:35 +0000

    incre logging latency

--- functions/trigger_bench.py
+++ functions/trigger_bench.py
@@ -78,6 +78,7 @@ elif 'run' in msg:
 		sent_msgs = 0
 		end_recv = 0
 		time.sleep(1.0)
+		utils.print_latency_stats(latency, 'Causal')
 	logging.info("benchmark done")
 	utils.print_latency_stats(latency, 'Causal', True)
 	utils.print_latency_stats(latency, 'Causal')
